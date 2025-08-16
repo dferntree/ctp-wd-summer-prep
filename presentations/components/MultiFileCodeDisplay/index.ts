8e376de48e9f1f5b413a67b00ec09a1b9ba637a6
dferntree <danielfernandez.ny@gmail.com>
2025-08-16 03:49:04 +0000

    Remove unwanted files, keep only habit tracker code

--- presentations/components/MultiFileCodeDisplay/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './types';
-export * from './utils';
-export * from './FileManager';
-export * from './ConsoleOutput';
-export * from './TestResults';
-export * from './OutputTabs';
-export * from './monacoConfig';
